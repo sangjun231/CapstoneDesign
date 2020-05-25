b310245e9ba4d42e3b59954fc0860472cdca8423
ji_hyeon <lovely2270@naver.com>
2020-05-26 00:21:20 +0900

    just_git연동

--- app/src/main/java/kjharu/com/capstone_2020/MainActivity.kt
+++ app/src/main/java/kjharu/com/capstone_2020/MainActivity.kt
@@ -7,6 +7,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        print("Hi");
         setContentView(R.layout.activity_main)
     }
 }
